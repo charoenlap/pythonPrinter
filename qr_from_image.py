01a55052417a7f57664125dd24a29d2061994fae
tikkub <Tikkubzaza@gmail.com>
2023-07-09 22:58:53 +0700

    dddcccddcc

--- qr_from_image.py
+++ qr_from_image.py
@@ -5,10 +5,62 @@ import requests
 import time
 import datetime
 import json
+from datetime import datetime
 printer_name = 'POS-80C1'
 
 def format_currency(amount):
     return "{:,.0f}".format(amount)
+
+def generate_receipt(items):
+    receipt = ""  # Initialize receipt string
+
+    # Get current date and time
+    current_datetime = datetime.now()
+
+    # Header line
+    receipt += "=" * 40 + "\n"
+    receipt += "{:^40}\n".format("ใบเสร็จ")
+    receipt += "=" * 40 + "\n"
+    receipt += "{:^40}\n".format(current_datetime.strftime("%d/%m/%Y %H:%M:%S"))
+    receipt += "-" * 40 + "\n"
+
+    total = 0  # Initialize total
+
+    # Print each item
+    for item in items:
+        name = item.get('name', '')
+        option = item.get('option', '')
+        price_str = item.get('price', '0.00')
+        price = float(price_str)
+        total += price
+
+        # Format and append item name and price without decimal places
+        formatted_price = "{:,.0f}".format(price)
+        receipt += "{:<35}{:>5}\n".format(name, formatted_price)
+
+        # Append item option on multiple lines with appropriate indentation
+        if option:
+            option_lines = option.split("\n")
+            for line in option_lines:
+                receipt += "  - {:<32}\n".format(line.strip())
+
+        receipt += "-" * 40 + "\n"
+
+    formatted_total = "{:,.0f}".format(total)
+    receipt += "{:<35}{:>5}\n".format("Total", formatted_total)
+    receipt += "=" * 40 + "\n"  # Footer line
+
+    return receipt
+
+
+# Example usage with multiple items
+items = [
+    {"name": "Item 1", "option": "Option A", "price": "9.99"},
+    {"name": "Item 2", "option": "This is a long option\nthat exceeds 26 characters", "price": "14.99"},
+    {"name": "Item 3", "option": "Option\nB", "price": "5.99"}
+]
+
+
 
 respDataReceipt = requests.get('http://tikkubzaza.trueddns.com:54242/web/restaurant/public_htmls/index.php?route=order/getReceipt')
 respDataReceipt = json.loads(respDataReceipt.content.decode('utf-8-sig'))
@@ -17,27 +69,8 @@ for receipt in respDataReceipt:
     respDataReceiptDetail = requests.get('http://tikkubzaza.trueddns.com:54242/web/restaurant/public_htmls/index.php?route=order/getOrder&table_id='+table_id)
     if respDataReceiptDetail.ok is not None:
         respDataReceiptDetail = json.loads(respDataReceiptDetail.content.decode('utf-8-sig'))
-        receipt = "***************************\n"
-        receipt += "                      RECEIPT        \n"
-        receipt += "************************************************\n\n"
-        receipt += "Items:\n"
-        max_item_length = max(len(item['name']) for item in respDataReceiptDetail)
-        total = 0
-        for item in respDataReceiptDetail:
-            item_line = "- {:<{item_width}}".format(item['name'], item_width=max_item_length)
-            price = float(item['price'])
-            receipt += f"{item_line}  {format_currency(price):>8} $\n"
-            receipt += "     - " + item['comment'] + "\n"
-            total += price
-        receipt += "\n"
-        receipt += "------------------------------------------------\n"
-        receipt += "Total:           {}\n".format(format_currency(total)) + ".-"
-        receipt += "------------------------------------------------\n\n"
+        receipt = generate_receipt(respDataReceiptDetail)
 
-        qr_code_content = "Your QR code text"  # Replace with your desired text for the QR code
-
-        receipt += "       ขอบคุณหลายเด้อ       \n"
-        receipt += "***********************************************\n"
 
         hDC = win32ui.CreateDC()
         hDC.CreatePrinterDC(printer_name)
