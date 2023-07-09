984705ead8dbe1b1f2d9b294165a175cbdb76271
tikkub <Tikkubzaza@gmail.com>
2023-07-09 20:29:53 +0700

    dddcccdd

--- print_all.py
+++ print_all.py
@@ -5,6 +5,7 @@ import requests
 import time
 import datetime
 import json
+import qrcode
 # from pythainlp.tokenize import dict_word_tokenize
 
 while True:
@@ -114,8 +115,8 @@ while True:
                     paper_width = 80 * 1440 / 25.4  # Convert 80mm to pixels
                     paper_height = 2000  # Adjust as needed
 
-                    # Start printing the receipt
-                    hDC.StartDoc("Receipt")
+                    # Start printing the order
+                    hDC.StartDoc("order")
                     hDC.StartPage()
 
                     font = win32ui.CreateFont({
@@ -133,7 +134,7 @@ while True:
                     x = 10  # Move the text closer to the left margin
                     y = 100
 
-                    # Print each line of the receipt
+                    # Print each line of the order
                     i = 0
                     for line in text.splitlines():
                         if i == 0:
@@ -167,6 +168,105 @@ while True:
                 for orderId in orderArr:
                     x = requests.get('https://charoenlap.com/restaurant/public_html/index.php?route=order/feedPrinterUpdate&order_id='+orderId)
                     print('https://charoenlap.com/restaurant/public_html/index.php?route=order/feedPrinterUpdate&order_id=' + orderId)
+            if printer_name == 'POS-80C1':
+                # data = json.loads(respData.content.decode('utf-8-sig'))
+                respDataReceipt = requests.get('http://tikkubzaza.trueddns.com:54242/web/restaurant/public_htmls/index.php?route=order/getReceipt')
+                for respDataReceipt in receipt:
+                    table = receipt.table_id
+                    respDataReceiptDetail = requests.get('http://tikkubzaza.trueddns.com:54242/web/restaurant/public_htmls/index.php?route=order/getOrder&table_id='+table_id)
+                    if respDataReceiptDetail.ok is not None:
+                        print(respDataReceiptDetail)
+                        receipt = "***************************\n"
+                        receipt += "        RECEIPT        \n"
+                        receipt += "***************************\n\n"
+                        receipt += "Items:\n"
+                        max_item_length = max(len(item['name']) for item in respDataReceiptDetail)
+                        total = 0
+                        for item in respDataReceiptDetail:
+                            item_line = "- {:<{item_width}}".format(item['name'], item_width=max_item_length)
+                            price = int(float(item['price']) * 100)
+                            receipt += f"{item_line}  ${price:>8}\n"
+                            receipt += "  Option: " + item['option'] + "\n"
+                            total += price
+                        receipt += "\n"
+                        receipt += "---------------------------\n"
+                        receipt += "        เลขบัญชี 1730691250 กรุงไทย        \n"
+                        receipt += "       ชื่อ นาย ชาตรี มณีวรวัฒน์       \n"
+                        receipt += "---------------------------\n\n"
+
+                        qr_code_content = "Your QR code text"  # Replace with your desired text for the QR code
+
+                        # Generate the QR code
+                        qr_code = qrcode.make(qr_code_content)
+
+                        # Add the QR code to the receipt
+                        receipt += qr_code.get_ascii() + "\n"
+
+                        receipt += "       Thank you for your purchase!       \n"
+                        receipt += "***************************\n"
+
+                        hDC = win32ui.CreateDC()
+                        hDC.CreatePrinterDC(printer_name)
+
+                        # Define the font properties
+                        font_name = "TH Sarabun New"
+                        font_size = 1.8  # Smaller font size
+                        font_weight = 800
+
+                        # Set up paper size
+                        paper_width = 80 * 1440 / 25.4  # Convert 80mm to pixels
+                        paper_height = 2000  # Adjust as needed
+
+                        # Start printing the order
+                        hDC.StartDoc("order")
+                        hDC.StartPage()
+
+                        font = win32ui.CreateFont({
+                            "name": font_name,
+                            "height": int(font_size * -20),  # Convert font size to logical units
+                            "weight": font_weight,
+                        })
+
+                        hDC.SelectObject(font)
+
+                        # Calculate the size of a character in the selected font
+                        char_width, char_height = hDC.GetTextExtent("X")
+
+                        # Define the initial x and y coordinates for printing
+                        x = 10  # Move the text closer to the left margin
+                        y = 100
+
+                        # Print each line of the order
+                        i = 0
+                        for line in receipt.splitlines():
+                            if i == 0:
+                                font = win32ui.CreateFont({
+                                    "name": font_name,
+                                    "height": int(3 * -20),  # Convert font size to logical units
+                                    "weight": font_weight,
+                                })
+                                hDC.SelectObject(font)
+                            else:
+                                font = win32ui.CreateFont({
+                                    "name": font_name,
+                                    "height": int(font_size * -20),  # Convert font size to logical units
+                                    "weight": font_weight,
+                                })
+                            hDC.SelectObject(font)
+                            i+=1
+                            hDC.TextOut(x, y, line)
+                            y += char_height
+
+                            # Check if we reached the end of the page
+                            if y + char_height > paper_height:
+                                hDC.EndPage()
+                                hDC.StartPage()
+                                y = 100  # Reset y coordinate to top of new page
+
+                        # End printing and clean up the DC
+                        hDC.EndPage()
+                        hDC.EndDoc()
+                        hDC.DeleteDC()
     x = datetime.datetime.now()
     print(x)
     time.sleep(2)
